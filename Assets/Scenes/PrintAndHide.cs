f165ac4342d176d916c17b06e10ec796e440145b
Chris <zealot626@hotmail.com>
2020-08-19 20:29:02 +1000

    100 percent complete

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -16,6 +16,15 @@ public class PrintAndHide : MonoBehaviour
     void Update()
     {
         Debug.Log("<"+gameObject.name+">:" + i);
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i >= 200 && i <= 250)
+        {
+            gameObject.SetActive(false);
+            rend.enabled = false;
+        }
         i++;
     }
 }
